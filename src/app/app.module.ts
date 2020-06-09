c40f721458957b6836070df28668caa54376aa08
ralphn@afrosoft.co.zw <ralphtashinga@gmail.com>
2020-06-09 03:25:21 +0200

    Adding html basic content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { OurTeamComponent } from './components/our-team/our-team.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactUsComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutUsComponent,
+    FooterComponent,
+    HeaderComponent,
+    OurTeamComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
